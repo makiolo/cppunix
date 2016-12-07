986036a10aaabea2666522ea59fc16eae7513912
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-12-07 22:02:07 +0100

    initial commit

---
+++ api.h
@@ -0,0 +1,39 @@
+#ifndef _FES_API_H_
+#define _FES_API_H_
+
+#define fes_VERSION_MAJOR 1
+#define fes_VERSION_MINOR 0
+#define fes_VERSION ((fes_VERSION_MAJOR << 16) | fes_VERSION_MINOR)
+
+#ifdef _MSC_VER
+    #ifdef fes_EXPORTS
+        #define fes_API __declspec(dllexport)
+    #else
+        #define fes_API __declspec(dllimport)
+    #endif
+#else
+    #ifdef fes_EXPORTS
+        #define fes_API __attribute__((visibility("default")))
+    #else
+        #define fes_API
+    #endif
+#endif
+
+using int64 = long long;
+using int32 = signed int;
+using int16 = signed short;
+using int8 = signed char;
+
+using uint64 = unsigned long long;
+using uint32 = unsigned int;
+using uint16 = unsigned short;
+using uint8 = unsigned char;
+
+using real64 = double;
+using real32 = float;
+
+using uint = uint32;
+using real = real32;
+
+#endif
+
