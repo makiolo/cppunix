e967c1fab2fa78f307bae5e12530f2b8aea604f5
Ricardo <makiolo@gmail.com>
2016-12-22 18:44:42 +0100

    Update test_channel.cpp

--- tests/test_channel.cpp
+++ tests/test_channel.cpp
@@ -79,12 +79,12 @@ TEST(ChannelTest, goroutines_or_something_like_that)
 	// channel
 	cu::channel<int> go(filter1, filter2, handler);
 	std::thread t1([&](){
-		for(int i=0; i<100000; ++i)
+		for(int i=0; i<1000; ++i)
 		{
 			go << i;
 		}
 	});
-	for(int i=0; i<100; ++i)
+	for(int i=0; i<1000; ++i)
 	{
 		std::cout << "recv: " << go.get() << std::endl;
 	}
