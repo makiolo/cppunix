635e90a89ccc4a36f18eb6175e95e364ab0fd9d0
Ricardo <makiolo@gmail.com>
2017-02-28 12:44:22 +0100

    Update test_shell.cpp

--- tests/test_shell.cpp
+++ tests/test_shell.cpp
@@ -52,7 +52,7 @@ TEST(CoroTest, TestGrep)
 }
 
 namespace cu {
-
+	
 class scheduler {
 public:
 	using control_type = void;
@@ -113,6 +113,15 @@ public:
 	
 	int getpid() const {return _pid;}
 	
+	inline void lock()
+	{
+		// cid id = whoiam_cpproutine()
+		// if(is.is_running())
+		// {
+		//	move id to blocked
+		// }
+	}
+	
 	// auto create_semaphore();
 	
 	// each semaphore have 2 channels and ret code
@@ -127,6 +136,68 @@ protected:
 private:
 	int _pid;
 };
+
+class semaphore
+{
+public:
+	explicit semaphore(scheduler& sche, int count = 0, int count_max = 1)
+		: _sche(sche)
+		, _count(count)
+		, _count_max(count_max)
+	{
+		assert((1 <= count_max) || (0 <= count));
+		assert(count <= count_max);
+	}
+
+	///
+	/// Reduce el valor del semaforo. Bloquea la regi�n critica. Esta operaci�n tiene m�ltiples
+	/// nombres.
+	///  * wait (s)
+	///	 * {
+	///		  if s > 0
+	///				s--
+	///		  else // s == 0
+	///				bloqueo
+	///		}
+	///
+	///		esperar / wait / lock / down / sleep / P
+	///
+	inline void lock()
+	{
+		if(_count > 0)
+		{
+			--_count;
+		}
+		else
+		{
+			_sche.lock(); // (*this)
+		}
+	}
+	///
+	/// Aumenta el semaforo. Libera la region critica.
+	///    signal(s)
+	///    {
+	///        if s == 0
+	///            s++
+	///        else // s > 0
+	///            if s < MAX
+	///                s++
+	///    }
+	///
+	///	avisar / signal / unlock / up / wakeup / release / V
+	///
+	inline void unlock()
+	{
+		if((_count == 0) || (_count < count_max))
+		{
+			++_count;
+		}
+	}
+protected:
+	scheduler& _sche;
+	int _count;
+	int _count_max;
+}
 	
 }
 
