a97061e69e7212764574143c2d347e8f93aa0a64
Ricardo <makiolo@gmail.com>
2017-03-08 16:31:09 +0100

    Update test_channel.cpp

--- tests/test_channel.cpp
+++ tests/test_channel.cpp
@@ -68,8 +68,8 @@ TEST(ChannelTest, goroutines_consumer)
 {
 	cu::scheduler sch;
 	cu::channel<std::string> go(sch);
-	go.connect(cu::quote("__^-^__"));
-	go.connect(cu::quote("__\o/__"));
+	// go.connect(cu::quote("__^-^__"));
+	// go.connect(cu::quote("__\o/__"));
 	sch.spawn([&](auto& yield) {
 		// send
 		for(int i=1; i<=200; ++i)
